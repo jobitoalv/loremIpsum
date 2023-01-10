339fd9ddf112ba5f8f766bfd251efe9ef67a3487
Job Alvarez <jobitoalv@gmail.com>
2023-01-10 11:55:08 +1000

    add handleSubmit function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { useState } from 'react';
 import data from './data';
+
 function App() {
-  
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
   
   return (
-  <h2>lorem ipsum project setup</h2>
+
+  <section className='section-center'>
+    <h3>tired of boring lorem ipsum?</h3>
+    <form className='lorem-form' onSubmit={handleSubmit}>
+      <lable htmlFor='amount'>
+        paragraphs:
+      </lable>
+      <input type='number' name='amount' id='amount'/>
+    </form>
+  </section>
     )
 }
 
